fc3e8320cdbfb9a5a80e5848003e7fb3a174dfee
João Pedro <jpmrc49@gmail.com>
2023-09-12 14:37:58 -0300

    style: change the colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,10 @@
-require('rose-pine').setup({
-    disable_background = true
+require('catppuccin').setup({
+    disable_background = true,
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
 })
 
 function ColorMyPencils(color) 
-	color = color or "rose-pine"
+	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -12,3 +13,4 @@ function ColorMyPencils(color)
 end
 
 ColorMyPencils()
+
